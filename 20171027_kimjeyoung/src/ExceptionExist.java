29c02a1a7deb4bab844e144e92a170f2d3b6332b
OHKKIM <31759437+OHKKIM@users.noreply.github.com>
2017-10-27 16:11:52 +0900

    2017.10.27

---
+++ 20171027_kimjeyoung/src/ExceptionExist.java
@@ -0,0 +1,13 @@
+
+public class ExceptionExist {
+	
+	public static void main(String[] args) {
+		func();
+	}
+	
+	static void func() {
+		int i = 1;
+		int j = 1;
+		System.out.println(i/j);
+	}
+}
